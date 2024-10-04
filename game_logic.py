0c9d449f0d6dae518962775cf2ec6236417d3320
Nivedhitha-Duggi <157204180+Nivedhitha-Duggi@users.noreply.github.com>
2024-10-03 20:03:20 -0500

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -4,22 +4,28 @@ class Game:
         self.inventory = []
 
     def collect_item(self, item):
-        """Collect an item and add it to the inventory."""
         self.inventory.append(item)
         self.items_collected += 1
-    
+        print(f"Collected: {item}")
+
     def recycle_item(self, item):
-    if item in self.inventory:
-        self.inventory.remove(item)
-        print(f"Recycled: {item}")
+            if item in self.inventory:
+            self.inventory.remove(item)
+            print(f"Recycled: {item}")
+        else:
+            print(f"Cannot recycle: {item} not found in inventory.")
 
     def view_inventory(self):
-    return self.inventory
-    
+         return self.inventory
+
     def craft_item(self, item_1, item_2):
-    if item_1 in self.inventory and item_2 in self.inventory:
-        crafted_item = f"{item_1} + {item_2}"
-        print(f"Crafted: {crafted_item}")
-        return crafted_item
-
-
+        if item_1 in self.inventory and item_2 in self.inventory:
+            crafted_item = f"{item_1} + {item_2}"
+            self.inventory.remove(item_1)
+            self.inventory.remove(item_2)
+            self.inventory.append(crafted_item)
+            print(f"Crafted: {crafted_item}")
+            return crafted_item
+        else:
+            print(f"Cannot craft: {item_1} or {item_2} not in inventory.")
+            return None
