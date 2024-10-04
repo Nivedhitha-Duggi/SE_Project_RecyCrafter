c62f6d88ddc3fcfa3e8bda4a1735f435ab57a1b5
Nivedhitha-Duggi <157204180+Nivedhitha-Duggi@users.noreply.github.com>
2024-10-03 19:58:37 -0500

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -7,9 +7,12 @@ class Game:
         """Collect an item and add it to the inventory."""
         self.inventory.append(item)
         self.items_collected += 1
+    
     def recycle_item(self, item):
     if item in self.inventory:
         self.inventory.remove(item)
+        print(f"Recycled: {item}")
+
     def view_inventory(self):
     return self.inventory
 
